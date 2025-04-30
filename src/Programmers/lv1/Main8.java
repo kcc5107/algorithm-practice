21d6271da1d2b820b839d832a5f87ac6c4b92358
Jang Gyeonghyuk <kcc5107@naver.com>
2025-04-30 10:03:32 +0900

    Lv1. 문자열을 정수로 바꾸기

---
+++ src/Programmers/lv1/Main8.java
@@ -0,0 +1,9 @@
+package Programmers.lv1;
+// 문자열을 정수로 바꾸기
+public class Main8 {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
