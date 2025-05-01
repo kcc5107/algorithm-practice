5e190a36b9100b6df75de6d7858e88ee5522a6a6
Jang Gyeonghyuk <kcc5107@naver.com>
2025-05-01 09:27:13 +0900

    Lv1. 정수 제곱근 판별

---
+++ src/Programmers/lv1/Main9.java
@@ -0,0 +1,16 @@
+package Programmers.lv1;
+// 정수 제곱근 판별
+public class Main9 {
+    public long solution(long n) {
+        long answer = -1;
+        if (n == 1) {
+            return 4;
+        }
+        for (long i = 2; i <= n / 2; i++) {
+            if (i * i == n) {
+                return (i + 1) * (i + 1);
+            }
+        }
+        return answer;
+    }
+}
