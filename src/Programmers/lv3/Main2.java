b48602896c1ecdba3c75090cb2fcf87822b9a7b4
Jang Gyeonghyuk <kcc5107@naver.com>
2025-05-26 20:57:14 +0900

    Lv3. 삼총사

---
+++ src/Programmers/lv3/Main2.java
@@ -0,0 +1,20 @@
+package Programmers.lv3;
+
+// 삼총사
+public class Main2 {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        for (int i = 0; i < number.length - 2; i++) {
+            for (int j = i + 1; j < number.length - 1; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
